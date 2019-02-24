dba2e612e08734a14cf35e04896e364ac9eb53bb
Mujeeb <mujib517@gmail.com>
2019-02-24 09:27:17 +0530

    Bug fix 404 error

--- controllers/book.ctrl.js
+++ controllers/book.ctrl.js
@@ -2,6 +2,8 @@ var books = [{ id: 1, name: 'Speaking Javascript', price: 100, inStock: true },
 { id: 2, name: 'Eloquent Javascript', price: 50, inStock: false },
 { id: 3, name: 'Headfirst Javascript', price: 80, inStock: true }];
 //CRUD
+// Truthy and Falsy
+// false, 0, null, undefined, "", NaN
 var bookCtrl = {
     get: function (req, res) {
         res.status(200);
@@ -9,14 +11,24 @@ var bookCtrl = {
     },
 
     getById: function (req, res) {
-        var id = req.params.id;
-        var bk;
+        // var id = parseInt(req.params.id);
+        var id = +req.params.id;
+        var bk; //undefined
         for (var i = 0; i < books.length; i++) {
-            if (books[i].id == id) bk = books[i];
+            if (books[i].id === id) bk = books[i];
         }
+        res.status(bk ? 200 : 404);
+        // res.json(bk ? bk : "Not found");
+        res.json(bk || "Not found");
 
-        res.status(200);
-        res.json(bk);
+        // if(bk){
+        //     res.status(200);
+        //     res.json(bk);
+        // }
+        // else{
+        //     res.status(404);
+        //     res.send("Not found");
+        // }
     },
 
     post: function (req, res) {
